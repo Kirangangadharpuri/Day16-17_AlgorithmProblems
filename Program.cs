42d9a06b03aa6c43b6f474764b4bfcac6c6c449e
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-07 00:42:35 -1200

    calling method using object and taking range

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("*****Welcome to the Algorithm Problems*****");
+            FindNumber obj = new FindNumber();
+            Console.WriteLine("Enter the limit:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Guess value B/W 0 to {N}");
+            obj.call(0, N - 1);
         }
     }
 }
