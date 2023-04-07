74e58e0d3aaabd319dd9e582468347d39a0a5979
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-07 00:24:36 -1200

    calling method using object

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("*****Welcome to the Algorithm Problems*****");
+            Console.WriteLine("\n");
+            Anagram a = new Anagram();
+            a.Check();
         }
     }
 }
