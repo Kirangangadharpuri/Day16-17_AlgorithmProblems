8bebfd774a4806a0417e86a50290e3c4b5fd85f7
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-06 23:51:50 -1200

    create object

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("*****Welcome to the Algorithm Problems*****");
+            Console.WriteLine("These are Ds and Algorithm Programs");
+            BinarySearchWord1 binarySearchWord = new BinarySearchWord1();
+            binarySearchWord.SearchWord(@"C:\Users\kiran\source\repos\AlgorithmPrograms\AlgorithmPrograms\File.txt.txt");
         }
     }
 }
