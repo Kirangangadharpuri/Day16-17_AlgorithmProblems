3b65216caf5dcc2173450303de8dfc57b986def9
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-07 00:30:46 -1200

    calling method using object

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("*****Welcome to the Algorithm Problems*****");
+            PrimeNumber primeNumber = new PrimeNumber();
+            primeNumber.Display();
         }
     }
 }
