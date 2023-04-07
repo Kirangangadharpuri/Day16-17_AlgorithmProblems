dc1c7d75cb2a62585feb4dc734c1c85e55638c64
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-07 00:03:34 -1200

    code for insertion sort

---
+++ InsertionSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class InsertionSort
+    {
+        public void Display()
+
+        {
+            string[] str = { "Kiran", "Komal", "Kanchan" };
+            int s = str.Length;
+            //string[] ch1 = str.ToLower().ToStringArray();
+
+            for (int i = 0; i < s; i++)
+            {
+
+                var flag = 0;
+                string temp = "";
+                for (int j = i + 1; j < s; j++)
+                {
+
+                    if (str[i].CompareTo(str[j]) > 0)
+                    {
+                        temp = str[i];
+                        str[i] = str[j];
+                        str[j] = temp;
+
+
+                    }
+
+
+                }
+
+
+            }
+            foreach (string d in str)
+            {
+                Console.WriteLine(d);
+            }
+
+
+
+        }
+    }
+}
