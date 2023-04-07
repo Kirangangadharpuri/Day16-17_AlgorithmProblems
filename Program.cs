531fc88b07e4903d5ffb973769b6b3c90d6498a9
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-07 00:17:43 -1200

    calling method

--- Program.cs
+++ Program.cs
@@ -5,6 +5,21 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("*****Welcome to the Algorithm Problems*****");
+            int[] numbers = { 38, 27, 43, 3, 0, 82, 10 };
+            int len = numbers.Length;
+            Console.WriteLine("Before Merge Sort:");
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("After Merge Sort");
+            Merge.SortMethod(numbers, 0, len - 1);
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Read();
         }
     }
 }
